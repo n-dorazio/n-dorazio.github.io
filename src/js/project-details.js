74649e1201ad0966dd73f02d0ba7d285b6096b51
n-dorazio <natedorazio@gmail.com>
2025-01-09 18:42:31 -0700

    created a project details template and started on the LiftLog details page

--- src/js/project-details.js
+++ src/js/project-details.js
@@ -3,6 +3,7 @@ import { initLottieBackground } from './backgroundAnimation.js';
 class ProjectDetails {
     constructor() {
         this.initializeNavLinks();
+        this.initializeMobileMenu();
         if (window.lottie) {
             initLottieBackground();
         }
@@ -17,6 +18,29 @@ class ProjectDetails {
                 window.location.href = href;
             });
         });
+    }
+
+    initializeMobileMenu() {
+        const menuToggle = document.querySelector('.menu-toggle');
+        const navLinks = document.querySelector('.nav-links');
+
+        if (menuToggle && navLinks) {
+            menuToggle.addEventListener('click', () => {
+                navLinks.classList.toggle('active');
+                const isOpen = navLinks.classList.contains('active');
+                menuToggle.querySelector('i').classList.toggle('fa-bars', !isOpen);
+                menuToggle.querySelector('i').classList.toggle('fa-times', isOpen);
+            });
+
+            // Close menu when clicking a link
+            navLinks.querySelectorAll('a').forEach(link => {
+                link.addEventListener('click', () => {
+                    navLinks.classList.remove('active');
+                    menuToggle.querySelector('i').classList.add('fa-bars');
+                    menuToggle.querySelector('i').classList.remove('fa-times');
+                });
+            });
+        }
     }
 }
 
